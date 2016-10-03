b2f23d69b78140f9f1d0504d0f0c4b6201e9ead6
Fritask <fritas.series@gmail.com>
2016-10-03 08:53:56 -0300

    Comecando a implementar o tabuleiro

---
+++ aulaxadrez/tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public cor cor { get; protected set; }
+        public int qtdeMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+
+        public Peca(Posicao posicao, Tabuleiro tab, cor cor)
+        {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.cor = cor;
+            this.qtdeMovimentos = 0;
+        }
+        
+
+
+    }
+}
